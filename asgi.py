af25f6df78488134703f4ae125403c5e89701f06
elekozlova <helena.kozlova@tut.by>
2021-10-08 21:21:06 +0300

    add folium

--- asgi.py
+++ asgi.py
@@ -1,12 +1,13 @@
-
+import folium
 from fastapi import FastAPI, Body
 
-from fastapi.responses import Response
+from fastapi.responses import Response, FileResponse
 from pydantic import BaseModel
 
 from my_fun import apply_cache_headers
 from my_fun import get_latlongs, get_distance
 from my_fun import static_response
+
 
 app = FastAPI()
 
@@ -19,12 +20,11 @@ async def _(response: Response):
 
     return static_response("my.html")
 
-
-@app.get("/img")
+@app.get("/map")
 async def _(response: Response):
     apply_cache_headers(response)
 
-    return static_response("image.jpg")
+    return static_response("map1.html")
 
 
 @app.get("/style")
@@ -47,9 +47,23 @@ class Points(BaseModel):
 
 
 @app.post("/distance")
-async def _(points: Points = Body(...)):
+async def _(response: Response  , points: Points = Body(...)):
     result = get_distance(get_latlongs(points.start), get_latlongs(points.end))
+    map = folium.Map(location=get_latlongs(points.start) , zoom_start = 8, tiles = "cartodbpositron")
+    for coordinates in [get_latlongs(points.start), get_latlongs(points.end)]:
+        folium.Marker(location=coordinates, icon=folium.Icon(color = 'cadetblue',icon="cloud")).add_to(map)
+    map.save("map1.html")
     return result
+
+@app.get("/map")
+async def _(response: Response):
+    apply_cache_headers(response)
+
+    return static_response("map1.html")
+
+
+
+
 
 
 
